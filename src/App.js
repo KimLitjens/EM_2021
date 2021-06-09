6ef637464ab3de2f0df22a7fb1c5bc10f7a31db4
Kim Litjens <Kimlitjens4@gmail.com>
2021-06-09 11:20:01 +0200

    Setup tailwind and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import * as ROUTES from './constants/routes'
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const Login = lazy(() => import('./pages/login'))
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>NederFrench EM 2021</h1>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+        </Switch>
+      </Suspense>
+    </Router>
+
   );
 }
 
-export default App;
---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+
+    return (
+        <header className="">
+            <h1 className="flex justify-center text-red-500 text-4xl">NederFrench EM 2021</h1>
+        </header>
+    )
+}
