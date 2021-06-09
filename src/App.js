f463bcb9c477a3a0a5989e68d0b42b9b9341841b
Kim Litjens <Kimlitjens4@gmail.com>
2021-06-09 19:48:35 +0200

    add profile page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 import * as ROUTES from './constants/routes'
 const Dashboard = lazy(() => import('./pages/dashboard'));
-const Login = lazy(() => import('./pages/login'))
+const Login = lazy(() => import('./pages/login'));
+const Profile = lazy(() => import('./pages/profile'));
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
           <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
         </Switch>
       </Suspense>
---
+++ src/pages/profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import { NextGamesContainer } from '../containers/nextGames'
+import { LastGamesContainer } from '../containers/lastGames'
+
+export default function Profile() {
+
+    return (
+        <div className="">
+            <h1>Profile Name</h1>
+        </div>
+    )
+}
