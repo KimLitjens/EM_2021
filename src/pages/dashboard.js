737802f5f42d8b90a263f2c9dcd7345be710cd83
Kim Litjens <Kimlitjens4@gmail.com>
2021-06-09 15:30:01 +0200

    add base style and matchData

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { matchData } from '../matchData'
 export default function Dashboard() {
-
+    const sortedMatchData = matchData.sort((a, b) => new Date(a.match_start) - new Date(b.match_start))
+    console.log(sortedMatchData)
     return (
         <header className="">
-            <h1 className="flex justify-center text-red-500 text-4xl">NederFrench EM 2021</h1>
+            <h1 className="flex justify-center text-red-500 text-4xl">
+                NederFrench EM 2021 (510)
+            </h1>
+            <h2 className="">Score:</h2>
+            <h3>Player 1:</h3>
+            <h3>Player 2:</h3>
+            <p className="">Last games:</p>
+            <p className="">Next games:</p>
+
         </header>
     )
 }
---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+
+    return (
+        <header className="">
+            <h1 className="flex justify-center text-blue-500 text-4xl">Login Page</h1>
+        </header>
+    )
+}
